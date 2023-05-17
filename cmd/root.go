ab88711bc01df3b0f9da38f213460b661134ff5f
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-05-17 15:51:25 +0100

    Added checkInitFile func to root hoster command

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -12,6 +13,10 @@ var rootCmd = &cobra.Command{
 	Short: "HosterCore is a highly opinionated Bhyve automation platform written in Go",
 
 	Run: func(cmd *cobra.Command, args []string) {
+		err := checkInitFile()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 		hostMain()
 		vmListMain()
 	},
