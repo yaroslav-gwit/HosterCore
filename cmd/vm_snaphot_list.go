ca09c02f09f22185a812feaf0a44e64642eb9f48
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-06-16 12:24:27 +0100

    snapshot list dev

--- cmd/vm_snaphot_list.go
+++ cmd/vm_snaphot_list.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -28,22 +32,47 @@ var (
 )
 
 type SnapshotInfo struct {
-	Name string
-	Size uint64
+	Name      string
+	SizeBytes uint64
+	SizeHuman string
 }
 
-func getSnapshotInfo(vmName string) (SnapshotInfo, error) {
-	snapshotInfo := SnapshotInfo{}
+func getSnapshotInfo(vmName string) ([]SnapshotInfo, error) {
+	vmList := getAllVms()
+	vmExists := false
+	for _, v := range vmList {
+		if v == vmName {
+			vmExists = true
+		}
+	}
+
+	if !vmExists {
+		return []SnapshotInfo{}, errors.New("vm was not found")
+	}
+
+	snapshotInfo := []SnapshotInfo{}
 	vmDataset, err := getVmDataset(vmName)
 	if err != nil {
-		return SnapshotInfo{}, err
+		return []SnapshotInfo{}, err
 	}
 
 	out, err := exec.Command("zfs", "list", "-rpH", "-t", "snapshot", "-o", "name,used", vmDataset).CombinedOutput()
-	fmt.Println("zfs", "list", "-rpH -t -o name,used", vmDataset)
-	fmt.Println(string(out))
 	if err != nil {
-		return SnapshotInfo{}, err
+		fmt.Println(string(out))
+		return []SnapshotInfo{}, err
+	}
+
+	reSplitSpace := regexp.MustCompile(`\s+`)
+	for _, v := range strings.Split(string(out), "\n") {
+		if len(v) < 1 {
+			continue
+		}
+		tempList := reSplitSpace.Split(v, -1)
+		tempInfo := SnapshotInfo{}
+		tempInfo.Name = tempList[0]
+		tempInfo.SizeBytes, _ = strconv.ParseUint(tempList[1], 10, 64)
+		tempInfo.SizeHuman = ByteConversion(int(tempInfo.SizeBytes))
+		snapshotInfo = append(snapshotInfo, tempInfo)
 	}
 
 	return snapshotInfo, nil
