f63bbd42e1044502ad32f737b5ce93c4cd948cc9
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-07-19 23:54:56 +0100

    dev `mbuffer`

--- mbuffer/mbuffer.go
+++ mbuffer/mbuffer.go
@@ -2,35 +2,31 @@ package main
 
 import (
 	"io"
-	"log"
 	"os"
 	"strconv"
 	"time"
 )
 
 func main() {
-	// Step 1: Parse environment variable for speed limit
-	speedLimitMBPerSecond := 100 // Default value
+	// Parse environment variable for speed limit
+	speedLimitMBPerSecond := 100 // Set the default limit value, 100MB in our case
 	speedLimitStr := os.Getenv("SPEED_LIMIT_MBS")
 	if len(speedLimitStr) > 0 {
 		speedLimit, err := strconv.Atoi(speedLimitStr)
 		if err == nil && speedLimit > 0 {
-			log.Println("The speed limit is set to " + speedLimitStr + "MB")
 			speedLimitMBPerSecond = speedLimit
 		}
-	} else {
-		log.Println("The speed limit is set to 100MB")
 	}
 
-	// Step 2: Set up buffer
+	// Set up buffer
 	bufferSize := 1024 * 1024 // Adjust the buffer size as per your requirements
 	buffer := make([]byte, bufferSize)
 
-	// Step 3: Initialize the limiter
+	// Initialize the limiter
 	limiter := time.Tick(time.Second)
 
 	for {
-		// Step 3: Read from stdin respecting the speed limit
+		// Read from stdin respecting the speed limit
 		startTime := time.Now()
 		bytesRead, err := os.Stdin.Read(buffer)
 		if err != nil {
@@ -45,24 +41,20 @@ func main() {
 		dataSizeMB := float64(bytesRead)
 		durationSeconds := elapsedTime.Seconds()
 
-		// Step 4: Wait for the limiter to control the speed
+		// Wait for the limiter to control the speed
 		for range limiter {
-			// Calculate the actual speed of data transfer
 			actualSpeedMBPerSecond := dataSizeMB / durationSeconds
-
-			// If the actual speed exceeds the limit, adjust the limiter to control the speed
 			if actualSpeedMBPerSecond > float64(speedLimitMBPerSecond) {
-				limiter = time.Tick(time.Second / time.Duration(speedLimitMBPerSecond) * 6 / 100)
+				limiter = time.Tick(time.Second / time.Duration(speedLimitMBPerSecond) * 60 / 1024)
 			}
 			break
 		}
 
-		// Step 5: Write to stdout
-		_, err = os.Stdout.Write(buffer[:bytesRead])
+		// Write to stdout
+		_, err = os.Stdout.Write(buffer[0:bytesRead])
 		if err != nil {
 			panic(err)
 		}
 	}
-
-	// Step 6: Finish writing and exit
+	// Finish writing and exit
 }
