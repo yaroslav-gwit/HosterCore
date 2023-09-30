231422266f66cff259b8074f8a0e9c63ba8ea8ed
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-09-30 21:14:03 +0100

    jail dev

--- cmd/jail_bootstrap.go
+++ cmd/jail_bootstrap.go
@@ -7,7 +7,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
+	"github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
 )
 
@@ -36,9 +38,14 @@ var (
 	}
 )
 
+// Downloads a FreeBSD release specified, to be used as a Jail base.
+//
+// Returns an `error` if something goes wrong. Starts and reports the download progress otherwise.
 func downloadJailArchives(release string, dataset string, excludeLib32 bool) error {
 	// FreeBSD Mirror to get the archives from
 	// https://download.freebsd.org/releases/amd64/
+
+	// Check if the release exists block
 	requestUrl := fmt.Sprintf("https://download.freebsd.org/releases/amd64/%s", release)
 	res, err := http.Get(requestUrl)
 	if err != nil {
@@ -50,21 +57,98 @@ func downloadJailArchives(release string, dataset string, excludeLib32 bool) err
 		return err
 	}
 
-	fmt.Printf("Body: %s", string(body))
-	fmt.Println()
-	fmt.Printf("Response code: %v", res.Status)
-	fmt.Println()
+	if res.StatusCode == 200 && strings.Contains(string(body), "base.txz") {
+		_ = 0
+	} else {
+		return fmt.Errorf("release was not found on the FreeBSD website (here): %s", requestUrl)
+	}
+
+	err = res.Body.Close()
+	if err != nil {
+		return err
+	}
+	// EOF Check if the release exists block
+
+	// Download base.txz
+	archiveName := "base.txz"
+	fsFileLocation := fmt.Sprintf("/tmp/%s", archiveName)
+
+	res, err = http.Get(fmt.Sprintf("%s/base.txz", requestUrl))
+	if err != nil {
+		return err
+	}
+
+	_ = os.Remove(fsFileLocation)
+	f, err := os.OpenFile(fsFileLocation, os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+
+	bar := progressbar.DefaultBytes(
+		res.ContentLength,
+		fmt.Sprintf(" 📥 Downloading an archive || %s || %s", archiveName, fsFileLocation),
+	)
+
+	bytesWritten, err := io.Copy(io.MultiWriter(f, bar), res.Body)
+	if err != nil {
+		return err
+	} else {
+		emojlog.PrintLogMessage(fmt.Sprintf("%s has been downloaded (%s)", archiveName, ByteConversion(int(bytesWritten))), emojlog.Changed)
+	}
 
 	err = res.Body.Close()
 	if err != nil {
 		return err
 	}
 
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+	// EOF Download base.txz
+
+	// Download lib32.txz
+	if excludeLib32 {
+		// skip lib32 download
+		_ = 0
+	} else {
+		archiveName = "lib32.txz"
+		fsFileLocation = fmt.Sprintf("/tmp/%s", archiveName)
+
+		res, err = http.Get(fmt.Sprintf("%s/base.txz", requestUrl))
+		if err != nil {
+			return err
+		}
+
+		_ = os.Remove(fsFileLocation)
+		f, err = os.OpenFile(fsFileLocation, os.O_CREATE|os.O_WRONLY, 0600)
+		if err != nil {
+			return err
+		}
+
+		bar = progressbar.DefaultBytes(
+			res.ContentLength,
+			fmt.Sprintf(" 📥 Downloading an archive || %s || %s", archiveName, fsFileLocation),
+		)
+
+		bytesWritten, err = io.Copy(io.MultiWriter(f, bar), res.Body)
+		if err != nil {
+			return err
+		} else {
+			emojlog.PrintLogMessage(fmt.Sprintf("%s has been downloaded (%s)", archiveName, ByteConversion(int(bytesWritten))), emojlog.Changed)
+		}
+
+		err = res.Body.Close()
+		if err != nil {
+			return err
+		}
+
+		err = f.Close()
+		if err != nil {
+			return err
+		}
+	}
+	// EOF Download lib32.txz
+
 	return nil
 }
-
-// func configReleaseString(release string) (releaseStringCorrect bool) {
-// 	correctReleases
-
-// 	return
-// }
