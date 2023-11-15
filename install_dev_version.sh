b8f5abdaefac72be951c515f81a4fbc6bc8a01be
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-11-15 22:24:51 +0000

    Typo fixes

--- install_dev_version.sh
+++ install_dev_version.sh
@@ -4,61 +4,61 @@
 export BUILD_ALL=yes
 if [[ -n $1 && $1 != all ]]; then BUILD_ALL=no; fi
 
-# RED='\e[0;31m'
-LIGHT_RED='\e[1;31m'
-# GREEN='\e[0;32m'
-LIGHT_GREEN='\e[1;32m'
-NC='\e[0m'
+# RED='\033[0;31m'
+LIGHT_RED='\033[1;31m'
+# GREEN='\033[0;32m'
+LIGHT_GREEN='\033[1;32m'
+NC='\033[0m'
 
 ERROR_TEXT="${LIGHT_RED}ERROR:${NC}"
 
 bash pull_changes.sh
 echo ""
 
-if ! bash build.sh; then echo "${ERROR_TEXT} Build process failed!" && exit 1; fi
+if ! bash build.sh; then echo -e "${ERROR_TEXT} Build process failed!" && exit 1; fi
 echo ""
 
-echo "${LIGHT_GREEN}=== Starting the installation process ===${NC}"
+echo -e "${LIGHT_GREEN}=== Starting the installation process ===${NC}"
 
 # 1
 if [[ $1 == 1 || $1 == "hoster" || ${BUILD_ALL} == yes ]]; then
-    cp -v hoster /opt/hoster-core/ 2>/dev/null || echo "${ERROR_TEXT} hoster binary is in use"
+    cp -v hoster /opt/hoster-core/ 2>/dev/null || echo -e "${ERROR_TEXT} hoster binary is in use"
 fi
 
 # 2
 if [[ $1 == 2 || $1 == "vm_supervisor" || ${BUILD_ALL} == yes ]]; then
-    cp -v vm_supervisor/vm_supervisor /opt/hoster-core/vm_supervisor_service 2>/dev/null || echo "${ERROR_TEXT} vm_supervisor_service binary is in use"
+    cp -v vm_supervisor/vm_supervisor /opt/hoster-core/vm_supervisor_service 2>/dev/null || echo -e "${ERROR_TEXT} vm_supervisor_service binary is in use"
 fi
 
 # 3
 if [[ $1 == 3 || ${BUILD_ALL} == yes ]]; then
-    cp -v self_update/self_update /opt/hoster-core/self_update_service 2>/dev/null || echo "${ERROR_TEXT} self_update_service binary is in use"
+    cp -v self_update/self_update /opt/hoster-core/self_update_service 2>/dev/null || echo -e "${ERROR_TEXT} self_update_service binary is in use"
 fi
 
 # 4
 if [[ $1 == 4 || ${BUILD_ALL} == yes ]]; then
-    cp -v dns_server/dns_server /opt/hoster-core/ 2>/dev/null || echo "${ERROR_TEXT} dns_server binary is in use"
+    cp -v dns_server/dns_server /opt/hoster-core/ 2>/dev/null || echo -e "${ERROR_TEXT} dns_server binary is in use"
 fi
 
 # 5
 if [[ $1 == 5 || ${BUILD_ALL} == yes ]]; then
-    cp -v mbuffer/mbuffer /opt/hoster-core/ 2>/dev/null || echo "${ERROR_TEXT} mbuffer binary is in use"
+    cp -v mbuffer/mbuffer /opt/hoster-core/ 2>/dev/null || echo -e "${ERROR_TEXT} mbuffer binary is in use"
 fi
 
 # 6
 if [[ $1 == 6 || ${BUILD_ALL} == yes ]]; then
-    cp -v node_exporter/node_exporter /opt/hoster-core/node_exporter_custom 2>/dev/null || echo "${ERROR_TEXT} node_exporter_custom binary is in use"
+    cp -v node_exporter/node_exporter /opt/hoster-core/node_exporter_custom 2>/dev/null || echo -e "${ERROR_TEXT} node_exporter_custom binary is in use"
 fi
 
 # 7
 if [[ $1 == 7 || ${BUILD_ALL} == yes ]]; then
-    cp -v rest_api/rest_api /opt/hoster-core/hoster_rest_api 2>/dev/null || echo "${ERROR_TEXT} hoster_rest_api binary is in use"
+    cp -v rest_api/rest_api /opt/hoster-core/hoster_rest_api 2>/dev/null || echo -e "${ERROR_TEXT} hoster_rest_api binary is in use"
 fi
 
 # 8
 if [[ $1 == 8 || ${BUILD_ALL} == yes ]]; then
-    cp -v ha_watchdog/ha_watchdog /opt/hoster-core/ 2>/dev/null || echo "${ERROR_TEXT} ha_watchdog binary is in use"
+    cp -v ha_watchdog/ha_watchdog /opt/hoster-core/ 2>/dev/null || echo -e "${ERROR_TEXT} ha_watchdog binary is in use"
 fi
 
-echo "${LIGHT_GREEN}==== Copied over executable files ====${NC}"
-echo "${LIGHT_GREEN}=== Installation process done ===${NC}"
+echo -e "${LIGHT_GREEN}==== Copied over executable files ====${NC}"
+echo -e "${LIGHT_GREEN}=== Installation process done ===${NC}"
