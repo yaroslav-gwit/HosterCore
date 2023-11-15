668fba96bc3c0bbd81e4b501e4beba89a986dffd
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-11-15 22:47:10 +0000

    Build scripts improvements

--- build.sh
+++ build.sh
@@ -1,65 +1,75 @@
-#!/usr/bin/env sh
-echo "=== Starting the build process ==="
+#!/usr/bin/env bash
 set -e
 
-GIT_INFO=$(git describe --tags); DATE_INFO=$(date '+%Y-%m-%d_%H-%M-%S')
-VERSION=${GIT_INFO}_COMPILED_ON_${DATE_INFO}  #; _=${VERSION}
+# RED='\033[0;31m'
+# LIGHT_RED='\033[1;31m'
+# GREEN='\033[0;32m'
+LIGHT_GREEN='\033[1;32m'
+NC='\033[0m'
+
+# ERROR_TEXT="${LIGHT_RED}ERROR:${NC}"
+
+echo -e "${LIGHT_GREEN}=== Starting the build process ===${NC}"
+
+GIT_INFO=$(git describe --tags)
+DATE_INFO=$(date '+%Y-%m-%d_%H-%M-%S')
+VERSION=${GIT_INFO}_COMPILED_ON_${DATE_INFO} #; _=${VERSION}
 # Set the RELEASE=true, to build the release version
-if test -z "${RELEASE}"; then 
-    echo "Building the DEV version of HosterCore..."
+if test -z "${RELEASE}"; then
+    echo -e "${LIGHT_GREEN}Building the DEV version of HosterCore${NC}"
     echo ""
 
-    echo "Building the hoster module..."
+    printf "Building the ${LIGHT_GREEN}hoster${NC} module ... "
     # go build -a -ldflags="-X HosterCore/cmd.HosterVersion=${VERSION}" -o hoster
     go build -ldflags="-X HosterCore/cmd.HosterVersion=${VERSION}" -o hoster
-else 
-    echo "Building the RELEASE version of HosterCore..."
+else
+    echo -e "${LIGHT_GREEN}Building the RELEASE version of HosterCore${NC}"
     echo ""
 
-    echo "Building the hoster module..."
+    printf "Building the ${LIGHT_GREEN}hoster${NC} module ... "
     go build -o hoster
 fi
-echo "Done"
+printf "${LIGHT_GREEN}Done${NC}\n"
 
-echo "Building the vm_supervisor_service module..."
+printf "Building the ${LIGHT_GREEN}vm_supervisor_service${NC} module ... "
 cd vm_supervisor/
 go build
-echo "Done"
+printf "${LIGHT_GREEN}Done${NC}\n"
 
-echo "Building the self_update_service module..."
+printf "Building the ${LIGHT_GREEN}self_update_service${NC} module ... "
 cd ..
 cd self_update/
 go build
-echo "Done"
+printf "${LIGHT_GREEN}Done${NC}\n"
 
-echo "Building the dns_server module..."
+printf "Building the ${LIGHT_GREEN}dns_server${NC} module ... "
 cd ..
 cd dns_server/
-go build 
-echo "Done"
+go build
+printf "${LIGHT_GREEN}Done${NC}\n"
 
-echo "Building the mbuffer limiter module..."
+printf "Building the ${LIGHT_GREEN}mbuffer${NC} limiter module ... "
 cd ..
 cd mbuffer/
-go build 
-echo "Done"
+go build
+printf "${LIGHT_GREEN}Done${NC}\n"
 
-echo "Building the node_exporter_custom module..."
+printf "Building the ${LIGHT_GREEN}node_exporter_custom${NC} module ... "
 cd ..
 cd node_exporter/
-go build 
-echo "Done"
+go build
+printf "${LIGHT_GREEN}Done${NC}\n"
 
-echo "Building the hoster_rest_api module..."
+printf "Building the ${LIGHT_GREEN}hoster_rest_api${NC} module ... "
 cd ..
 cd rest_api/
-go build 
-echo "Done"
+go build
+printf "${LIGHT_GREEN}Done${NC}\n"
 
-echo "Building the ha_watchdog module..."
+printf "Building the ${LIGHT_GREEN}ha_watchdog${NC} module ... "
 cd ..
 cd ha_watchdog/
-go build 
-echo "Done"
+go build
+printf "${LIGHT_GREEN}Done${NC}\n"
 
-echo "=== Build process done ==="
+echo -e "${LIGHT_GREEN}=== Build process done ===${NC}"
--- install_dev_version.sh
+++ install_dev_version.sh
@@ -60,5 +60,4 @@ if [[ $1 == 8 || ${BUILD_ALL} == yes ]]; then
     cp -v ha_watchdog/ha_watchdog /opt/hoster-core/ 2>/dev/null || echo -e "${ERROR_TEXT} ha_watchdog binary is in use"
 fi
 
-echo -e "${LIGHT_GREEN}==== Copied over executable files ====${NC}"
 echo -e "${LIGHT_GREEN}=== Installation process done ===${NC}"
