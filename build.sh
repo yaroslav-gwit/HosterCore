aaf6ec3bb102b2a042aea682d7c31da5e8cd8992
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-11-15 22:50:28 +0000

    Typo fixes

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 # RED='\033[0;31m'
 # LIGHT_RED='\033[1;31m'
-# GREEN='\033[0;32m'
+GREEN='\033[0;32m'
 LIGHT_GREEN='\033[1;32m'
 NC='\033[0m'
 
@@ -16,60 +16,60 @@ DATE_INFO=$(date '+%Y-%m-%d_%H-%M-%S')
 VERSION=${GIT_INFO}_COMPILED_ON_${DATE_INFO} #; _=${VERSION}
 # Set the RELEASE=true, to build the release version
 if test -z "${RELEASE}"; then
-    echo -e "${LIGHT_GREEN}Building the DEV version of HosterCore${NC}"
+    echo -e "${GREEN}Building the DEV version of HosterCore${NC}"
     echo ""
 
-    printf "Building the ${LIGHT_GREEN}hoster${NC} module ... "
+    printf "Building the ${GREEN}hoster${NC} module ... "
     # go build -a -ldflags="-X HosterCore/cmd.HosterVersion=${VERSION}" -o hoster
     go build -ldflags="-X HosterCore/cmd.HosterVersion=${VERSION}" -o hoster
 else
-    echo -e "${LIGHT_GREEN}Building the RELEASE version of HosterCore${NC}"
+    echo -e "${GREEN}Building the RELEASE version of HosterCore${NC}"
     echo ""
 
-    printf "Building the ${LIGHT_GREEN}hoster${NC} module ... "
+    printf "Building the ${GREEN}hoster${NC} module ... "
     go build -o hoster
 fi
-printf "${LIGHT_GREEN}Done${NC}\n"
+printf "${GREEN}Done${NC}\n"
 
-printf "Building the ${LIGHT_GREEN}vm_supervisor_service${NC} module ... "
+printf "Building the ${GREEN}vm_supervisor_service${NC} module ... "
 cd vm_supervisor/
 go build
-printf "${LIGHT_GREEN}Done${NC}\n"
+printf "${GREEN}Done${NC}\n"
 
-printf "Building the ${LIGHT_GREEN}self_update_service${NC} module ... "
+printf "Building the ${GREEN}self_update_service${NC} module ... "
 cd ..
 cd self_update/
 go build
-printf "${LIGHT_GREEN}Done${NC}\n"
+printf "${GREEN}Done${NC}\n"
 
-printf "Building the ${LIGHT_GREEN}dns_server${NC} module ... "
+printf "Building the ${GREEN}dns_server${NC} module ... "
 cd ..
 cd dns_server/
 go build
-printf "${LIGHT_GREEN}Done${NC}\n"
+printf "${GREEN}Done${NC}\n"
 
-printf "Building the ${LIGHT_GREEN}mbuffer${NC} limiter module ... "
+printf "Building the ${GREEN}mbuffer${NC} limiter module ... "
 cd ..
 cd mbuffer/
 go build
-printf "${LIGHT_GREEN}Done${NC}\n"
+printf "${GREEN}Done${NC}\n"
 
-printf "Building the ${LIGHT_GREEN}node_exporter_custom${NC} module ... "
+printf "Building the ${GREEN}node_exporter_custom${NC} module ... "
 cd ..
 cd node_exporter/
 go build
-printf "${LIGHT_GREEN}Done${NC}\n"
+printf "${GREEN}Done${NC}\n"
 
-printf "Building the ${LIGHT_GREEN}hoster_rest_api${NC} module ... "
+printf "Building the ${GREEN}hoster_rest_api${NC} module ... "
 cd ..
 cd rest_api/
 go build
-printf "${LIGHT_GREEN}Done${NC}\n"
+printf "${GREEN}Done${NC}\n"
 
-printf "Building the ${LIGHT_GREEN}ha_watchdog${NC} module ... "
+printf "Building the ${GREEN}ha_watchdog${NC} module ... "
 cd ..
 cd ha_watchdog/
 go build
-printf "${LIGHT_GREEN}Done${NC}\n"
+printf "${GREEN}Done${NC}\n"
 
 echo -e "${LIGHT_GREEN}=== Build process done ===${NC}"
