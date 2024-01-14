f21691eeaa14585748cd30543b7e77ba6e4f89c2
Yaroslav Koisa <yaroslav@gateway-it.com>
2024-01-14 20:20:39 +0000

    debug

--- internal/pkg/hoster/jail/list_extended_table.go
+++ internal/pkg/hoster/jail/list_extended_table.go
@@ -52,6 +52,7 @@ func ListAllExtendedTable() (r []JailListExtendedTable, e error) {
 
 		jailConfig, err := GetJailConfig(jailDsFolder)
 		if err != nil {
+			fmt.Println(err)
 			continue
 		}
 
@@ -85,6 +86,7 @@ func ListAllExtendedTable() (r []JailListExtendedTable, e error) {
 
 		release, err := ReleaseVersion(jailDsFolder)
 		if err != nil {
+			fmt.Println(err)
 			continue
 		}
 		jailStruct.Release = release
