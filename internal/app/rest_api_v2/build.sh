33c3172551882a4d4ba028bde0f9ec5214cdf7b5
Yaroslav Koisa <yaroslav@gateway-it.com>
2024-03-02 18:02:44 +0000

    api2 dev

--- internal/app/rest_api_v2/build.sh
+++ internal/app/rest_api_v2/build.sh
@@ -2,5 +2,5 @@
 # This is a temporary build file for the separate APIv2 module.
 # Don't forget to execute `swag init --pd` to generate the docs prior to the pull.
 git pull
-go121 build -trimpath -o ./rest_api_v2 main.go
+go121 build -trimpath -o ./rest_api_v2
 ./rest_api_v2
