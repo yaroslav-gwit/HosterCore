d6fa0c3b11712d47b56ad1383ca72b7d2cf72824
Yaroslav Koisa <yaroslav@gateway-it.com>
2024-03-19 23:46:06 +0000

    refactoring

--- internal/app/scheduler/client/add_replication_job.go
+++ internal/app/scheduler/client/add_replication_job.go
@@ -160,7 +160,7 @@ func Replicate(job SchedulerUtils.ReplicationJob) error {
 
 	var replicateCmds []string
 	if len(remoteDs) < 1 {
-		cmd := fmt.Sprintf("zfs send -v -p %s | ssh -oBatchMode=yes -i %s -p%d %s zfs receive %s", toReplicate[0], job.SshKey, job.SshPort, job.SshEndpoint, localDs)
+		cmd := fmt.Sprintf("zfs send -vP %s | ssh -oBatchMode=yes -i %s -p%d %s zfs receive %s", toReplicate[0], job.SshKey, job.SshPort, job.SshEndpoint, localDs)
 		replicateCmds = append(replicateCmds, cmd)
 
 		fmt.Println(replicateCmds)
@@ -172,7 +172,7 @@ func Replicate(job SchedulerUtils.ReplicationJob) error {
 		if i >= len(toReplicate)-1 {
 			break
 		}
-		cmd := fmt.Sprintf("zfs send -v -i -p %s %s", v, toReplicate[i+1])
+		cmd := fmt.Sprintf("zfs send -viP %s %s", v, toReplicate[i+1])
 		replicateCmds = append(replicateCmds, cmd)
 	}
 
