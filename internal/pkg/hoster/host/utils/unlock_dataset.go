6d9cf54910d92cf81bd3dd070d8133d1210a8575
Yaroslav Koisa <yaroslav@gateway-it.com>
2024-05-20 23:58:22 +0100

    More error value improvements

--- internal/pkg/hoster/host/utils/unlock_dataset.go
+++ internal/pkg/hoster/host/utils/unlock_dataset.go
@@ -9,13 +9,6 @@ import (
 )
 
 func UnlockEncryptedDataset(dataset string, password string) (e error) {
-	// out, err := exec.Command("zfs", "load-key", dataset).CombinedOutput()
-	// if err != nil {
-	// 	e = fmt.Errorf("could not load the key for the dataset: %s", strings.TrimSpace(string(out)))
-	// 	return
-	// }
-
-	// cmd := exec.Command("zfs", "mount", "-l", "tank/vm-encrypted")
 	cmd := exec.Command("zfs", "load-key", dataset)
 
 	// Create a pipe to the command's stdin
@@ -29,7 +22,7 @@ func UnlockEncryptedDataset(dataset string, password string) (e error) {
 	cmd.Stderr = &stderr
 	// Start the command
 	if err := cmd.Start(); err != nil {
-		e = fmt.Errorf("could not start the load-key command: %s", err.Error())
+		e = fmt.Errorf("could not start the load-key command: %s; %s", strings.TrimSpace(stderr.String()), err.Error())
 		return
 	}
 	// Write the password to stdin and close it
@@ -39,10 +32,11 @@ func UnlockEncryptedDataset(dataset string, password string) (e error) {
 	}()
 	// Wait for the command to finish
 	if err := cmd.Wait(); err != nil {
-		e = fmt.Errorf("could not load the encryption key: %s; %s", stderr.String(), err.Error())
+		e = fmt.Errorf("could not load the encryption key: %s; %s", strings.TrimSpace(stderr.String()), err.Error())
 		return
 	}
 
+	// Mount the dataset
 	out, err := exec.Command("zfs", "mount", dataset).CombinedOutput()
 	if err != nil {
 		e = fmt.Errorf("could not mount dataset: %s", strings.TrimSpace(string(out)))
