c8218b825849a16b078ba8fd8f3ef8d53e179842
Yaroslav Koisa <yaroslav@gateway-it.com>
2024-07-05 22:46:42 +0100

    chore: Write cache for HosterVmUtils and HosterJailUtils in SnapshotClone handler

--- internal/app/rest_api_v2/pkg/handlers/snapshots.go
+++ internal/app/rest_api_v2/pkg/handlers/snapshots.go
@@ -423,6 +423,17 @@ func SnapshotClone(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	_, err = HosterVmUtils.WriteCache()
+	if err != nil {
+		ReportError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	_, err = HosterJailUtils.WriteCache()
+	if err != nil {
+		ReportError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
 	payload, _ := JSONResponse.GenerateJson(w, "message", "success")
 	SetStatusCode(w, http.StatusOK)
 	w.Write(payload)
