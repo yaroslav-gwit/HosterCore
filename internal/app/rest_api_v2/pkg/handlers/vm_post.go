55c4a855b32782dff71c4ee815c6f0c1e0f83992
Yaroslav Koisa <yaroslav@gateway-it.com>
2024-08-14 23:25:58 +0100

    Refactor VmPostRamInfo handler to set RAM configuration and log details

--- internal/app/rest_api_v2/pkg/handlers/vm_post.go
+++ internal/app/rest_api_v2/pkg/handlers/vm_post.go
@@ -372,8 +372,8 @@ func VmPostRamInfo(w http.ResponseWriter, r *http.Request) {
 	}
 
 	config.Memory = overallRamHuman
-	log.Debug("Setting RAM to: " + overallRamHuman)
-	log.Debug(config)
+	// log.Debug("Setting RAM to: " + overallRamHuman)
+	// log.Debug(config)
 
 	err = HosterVmUtils.ConfigFileWriter(config, location+"/"+HosterVmUtils.VM_CONFIG_NAME)
 	if err != nil {
